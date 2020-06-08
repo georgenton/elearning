6f105a7d4ec33df25b3d46239460c2848230bc98
root <root@smtp.millenialsoft.com>
2020-06-08 19:39:25 +0000

    pay-offline

---
+++ application/models/addons/Offline_payment_model.php
@@ -0,0 +1,89 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Offline_payment_model extends CI_Model {
+
+	function __construct()
+	{
+	    parent::__construct();
+ 	}
+
+ 	//Offline checkout User panel
+    public function attach_payment_document($file_extension = "") {
+        $total_amount = $this->session->userdata('total_price_of_checking_out');
+    	$user_id = $this->session->userdata('user_id');
+        $curse_id = json_encode($this->session->userdata('cart_items'));
+
+        $data['user_id'] = $user_id;
+        $data['amount'] = $total_amount;
+        $data['course_id'] = $curse_id;
+        $data['document_image'] = rand(6000, 10000000).'.'.$file_extension;
+        $data['timestamp'] = strtotime(date('H:i'));
+        $data['status'] = 0;
+
+        $this->db->insert('offline_payment', $data);
+        move_uploaded_file($_FILES['payment_document']['tmp_name'], 'uploads/payment_document/'.$data['document_image']);
+
+        $this->session->set_userdata('cart_items', array());
+    }
+
+    //User panel
+    public function pending_offline_payment($user_id = ""){
+    	if($user_id > 0){
+    		$this->db->where('user_id', $user_id);
+    	}
+    	$this->db->where('status', 0);
+    	return $this->db->get('offline_payment');
+    }
+
+
+
+
+    //Admin panel
+    public function offline_payment_all_data($offline_payment_id = ""){
+    	if($offline_payment_id > 0){
+    		$this->db->where('id', $offline_payment_id);
+    	}
+    	return $this->db->get('offline_payment');
+    }
+    public function offline_payment_pending($offline_payment_id = ""){
+    	if($offline_payment_id > 0){
+    		$this->db->where('id', $offline_payment_id);
+    	}
+    	$this->db->order_by('id', 'ASC');
+    	$this->db->where('status', 0);
+    	return $this->db->get('offline_payment');
+    }
+    public function offline_payment_approve($offline_payment_id = ""){
+    	if($offline_payment_id > 0){
+    		$this->db->where('id', $offline_payment_id);
+    	}
+    	$this->db->order_by('id', 'ASC');
+    	$this->db->where('status', 1);
+    	return $this->db->get('offline_payment');
+    }
+    public function offline_payment_suspended($offline_payment_id = ""){
+    	if($offline_payment_id > 0){
+    		$this->db->where('id', $offline_payment_id);
+    	}
+    	$this->db->order_by('id', 'ASC');
+    	$this->db->where('status', 2);
+    	return $this->db->get('offline_payment');
+    }
+
+
+    public function approve_offline_payment($param1 = ""){
+    	$this->db->where('id', $param1);
+    	$this->db->update('offline_payment', array('status' => 1));
+    }
+    public function suspended_offline_payment($param1 = ""){
+    	$this->db->where('id', $param1);
+    	$this->db->update('offline_payment', array('status' => 2));
+    }
+    public function delete_offline_payment($param1 = ""){
+    	$this->db->where('id', $param1);
+    	$this->db->delete('offline_payment');
+    }
+
+
+}
---
+++ application/views/payment/offline_payment_gateway.php
@@ -0,0 +1,50 @@
+<style>
+	.offline-form{
+		display: none;
+	}
+
+	.payment-gateway-icon-offline{
+		height: 32px;
+		float: right;
+		margin-right: 25%;
+	}
+	.offline_payment_instruction{
+		padding: 20px;
+		min-height: 100px;
+		width: 100%;
+		background-color: #E6E9FC;
+		margin-top: 100px;
+		border-radius: 10px;
+	}
+
+	@media only screen and (max-width: 600px) {
+	  .offline{
+	    margin-left: 5px;
+	    width: 70%;
+	  }
+	}
+
+</style>
+
+<div class="row payment-gateway offline" onclick="selectedOfflinePaymentGateway()">
+	<div class="col-12">
+		<img class="tick-icon offline-icon" src="<?php echo base_url('assets/payment/tick.png'); ?>">
+		<img class="payment-gateway-icon-offline" src="<?php echo base_url('assets/payment/offline_payment.png'); ?>">
+	</div>
+</div>
+
+<script>
+	function selectedOfflinePaymentGateway(){
+		$(".offline").css("border","2px solid #00D04F");
+		$('.offline-icon').show();
+		$('.offline-form').show();
+
+		$(".paypal").css("border","2px solid #D3DCDD");
+		$('.paypal-icon').hide();
+		$('.paypal-form').hide();
+
+		$(".stripe").css("border","2px solid #D3DCDD");
+		$('.stripe-icon').hide();
+		$('.stripe-form').hide();
+	}
+</script>
